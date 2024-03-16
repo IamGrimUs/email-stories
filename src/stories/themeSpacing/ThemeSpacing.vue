34e68c72f239969a5fa29f61d1771620ece0176b
r_dbl_l <iamgrimus@gmail.com>
2024-03-15 21:30:14 -0700

    add theme spacing without docs

---
+++ src/stories/themeSpacing/ThemeSpacing.vue
@@ -0,0 +1,31 @@
+<template>
+  <div :class="classes" :style="style">{{ `${spacing}px` }}</div>
+</template>
+
+<script>
+import './ThemeSpacing.css'
+import { reactive, computed } from 'vue'
+
+export default {
+  name: 'CallToAction',
+
+  props: {
+    spacing: {
+      type: Number
+    }
+  },
+  setup(props) {
+    props = reactive(props)
+    return {
+      classes: computed(() => ({
+        'storybook-spacing': true,
+        [`storybook-button--${props.spacing || 10}`]: true
+      })),
+      style: computed(() => ({
+        height: `${props.spacing}px`,
+        fontSize: `${props.spacing / 2}px`
+      }))
+    }
+  }
+}
+</script>
